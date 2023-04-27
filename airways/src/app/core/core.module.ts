a496756b2c3b9fab4a84d89f558c12258493b716
webbomj <nethack@mail.ru>
2023-04-27 17:02:53 +0300

    feat: add auth form

--- airways/src/app/core/core.module.ts
+++ airways/src/app/core/core.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Page404Component } from './pages/page404/page404.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { AuthComponent } from './components/auth/auth.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, Page404Component],
+  declarations: [HeaderComponent, FooterComponent, Page404Component, AuthComponent, LoginComponent, SignupComponent],
   imports: [
     SharedModule,
     RouterModule.forChild([{ path: '', component: Page404Component }]),
