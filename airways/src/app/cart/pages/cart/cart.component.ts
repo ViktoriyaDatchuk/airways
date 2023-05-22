13a6e83a1754828c8d7be3548a9ac115d25b24b3
Viktoriya Datchuk <grelada@tut.by>
2023-05-22 14:39:41 +0300

    feat: add payment row

--- airways/src/app/cart/pages/cart/cart.component.ts
+++ airways/src/app/cart/pages/cart/cart.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { trips } from '../../tripsmock';
+import { IDataTravel } from '../../tripsmock';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
 })
-export class CartComponent {}
+export class CartComponent implements OnInit {
+  public trips: IDataTravel[] = trips;
+
+  public sum: number = 0;
+
+  ngOnInit(): void {
+    this.sum = this.trips.reduce((sum, trip) => sum + trip.price, 0);
+  }
+}
